76de424a4eb130f5ff2a6d709170c69f3346ac69
bluestar <spamcollectorelextraordinaire@gmail.com>
2026-04-05 04:45:11 -0700

    Added a version note to Output.js too
    to help with debugging on the off chance this comes up

--- src/output.js
+++ src/output.js
@@ -1,3 +1,5 @@
+// Last updated with Brute Force Ozone Buster V.1.0
+
 const modifier = (text) => {
     text = ozoneBuster(text); // Add this line somewhere in the output modifier function
     return { text }
